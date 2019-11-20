446ebea2b3dd3438e1fa6df1efbcd9ed676f4faa
Robert M Ricci <rmricci121@gmail.com>
2019-11-20 15:21:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,3 @@
 def reverse_each_word(string)
-   string = string.split('')
- reversed_string = []
- 
- string.each do |char|
-  reversed_string.unshift(char)
- end
- 
- return reversed_string.join('')
+   
 end

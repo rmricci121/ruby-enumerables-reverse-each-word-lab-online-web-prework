b0ce07c249b91087ad0cfd8f2fc447ba58547596
Robert M Ricci <rmricci121@gmail.com>
2019-11-20 15:21:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,10 @@
 def reverse_each_word(string)
-  array = [string]
-  
+   string = string.split('')
+ reversed_string = []
+ 
+ string.each do |char|
+  reversed_string.unshift(char)
+ end
+ 
+ return reversed_string.join('')
 end

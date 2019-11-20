af9bbf60458ad1fd5dc4bc305f73abac8ab7dac5
Robert M Ricci <rmricci121@gmail.com>
2019-11-20 15:30:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
    i = 0 
-   array = [string]
-   while i < array.length do
+   #array = [string]
+   while i < string.length do
      string.reverse 
      i += 1 
   end

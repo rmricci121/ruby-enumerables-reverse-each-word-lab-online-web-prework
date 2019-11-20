bbb35ead3163f367d713a7909410a682234c95cc
Robert M Ricci <rmricci121@gmail.com>
2019-11-20 16:06:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(string)
-  original_array = string.split(" ")
-  return_array = []
-  original_array.each do|string|
-    return_array << string.reverse
-  end
-  return_array.join(" ")
-end
+# def reverse_each_word(string)
+#   original_array = string.split(" ")
+#   return_array = []
+#   original_array.each do|string|
+#     return_array << string.reverse
+#   end
+#   return_array.join(" ")
+# end
 
 def reverse_each_word_with_collect(string)
   array = string.split(" ") #turn string into an array
